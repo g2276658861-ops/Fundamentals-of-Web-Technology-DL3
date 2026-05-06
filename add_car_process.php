73081efe075f2fa6df7f8b824d8a52fdda173942
Lifucheng666-richman <3098133702@qq.com>
2026-05-06 17:09:20 +0800

    Delete add_car_process.php

--- add_car_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('Location: add-car.php');
-    exit();
-}
-
-echo 'Add car form received. Database insert will be added in the next commit.';
-?>
